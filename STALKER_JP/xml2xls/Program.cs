4dd2250109f4eb54f06c0839e22c43b9b20373b4
sisizanohito <sisizanohito@gmail.com>
2018-12-28 06:08:07 +0900

    デシリアライズしてxmlを読み込む

--- STALKER_JP/xml2xls/Program.cs
+++ STALKER_JP/xml2xls/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace xml2xls
 {
@@ -10,6 +12,37 @@ namespace xml2xls
     {
         static void Main(string[] args)
         {
+
+            //保存元のファイル名
+            string fileName = @"";
+
+            //XmlSerializerオブジェクトを作成
+            System.Xml.Serialization.XmlSerializer serializer =
+                new System.Xml.Serialization.XmlSerializer(typeof(uiText));
+
+            //読み込むファイルを開く
+            System.IO.StreamReader sr = new System.IO.StreamReader(fileName, new System.Text.UTF8Encoding(false));
+            //XMLファイルから読み込み、逆シリアル化する
+            uiText obj = (uiText)serializer.Deserialize(sr);
+            //ファイルを閉じる
+            sr.Close();
         }
     }
+
+    [XmlRoot("string_table")]
+    public class uiText
+    {
+        [XmlElement("string")]
+        public List<Texts> uiTexts { get; set; }
+    }
+
+    public class Texts
+    {
+        [XmlAttribute("id")]
+        public string ID { get; set; }
+        [XmlElement("text")]
+        public string text { get; set; }
+    }
+
+
 }
