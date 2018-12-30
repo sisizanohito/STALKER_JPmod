a67b0be066a01d9a2d01051e20ec7146652f384d
sisizanohito <sisizanohito@gmail.com>
2018-12-31 03:01:28 +0900

    xmlの読み込み xlsxの吐き出し

--- STALKER_JP/xml2xls/Program.cs
+++ STALKER_JP/xml2xls/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
+using ClosedXML.Excel;
 
 namespace xml2xls
 {
@@ -12,28 +13,71 @@ namespace xml2xls
     {
         static void Main(string[] args)
         {
+            var workbook = new XLWorkbook();
+            var topWorksheet = workbook.Worksheets.Add(@"日本語化");
 
             //保存元のファイル名
-            string fileName = @"";
+            string folderName = @"./eng/";
 
+            System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(folderName);
+            System.IO.FileInfo[] files =
+                di.GetFiles("*.xml", System.IO.SearchOption.AllDirectories);
+
+            //ListBox1に結果を表示する
+            foreach (System.IO.FileInfo f in files)
+            {
+                uiText data = ReadXML(f.FullName);
+                string sheetName = NormalizeLength(f.Name.Replace(f.Extension, ""),31);
+                var worksheet = workbook.Worksheets.Add(sheetName);
+                worksheet.Cell("A1").Value = f.Name.Replace(f.Extension, "");
+
+                //トップにシートのリンクを制作
+                var topCell = topWorksheet.Cell(workbook.Worksheets.Count, 1);
+                topCell.Value= f.Name.Replace(f.Extension, "");
+                topCell.Hyperlink = new XLHyperlink($"'{sheetName}'!A1");
+
+            }
+            workbook.SaveAs(@"Stalker日本語化.xlsx");
+            
+        }
+
+        static string NormalizeLength(string value, int maxLength)
+        {
+            if(value.Length > maxLength)
+            {
+                return value.Substring(0, maxLength);
+            }
+            return value;
+
+        }
+
+        static public uiText ReadXML(string fileName)
+        {
             //XmlSerializerオブジェクトを作成
-            System.Xml.Serialization.XmlSerializer serializer =
-                new System.Xml.Serialization.XmlSerializer(typeof(uiText));
+            XmlSerializer serializer =
+                new XmlSerializer(typeof(uiText));
 
             //読み込むファイルを開く
-            System.IO.StreamReader sr = new System.IO.StreamReader(fileName, new System.Text.UTF8Encoding(false));
+            StreamReader sr = new StreamReader(fileName, Encoding.GetEncoding("windows-1251"));
+
+            string str = sr.ReadToEnd();
+            //Console.WriteLine(str);
+            str = str.Replace("&", "[#]");
             //XMLファイルから読み込み、逆シリアル化する
-            uiText obj = (uiText)serializer.Deserialize(sr);
+            uiText obj = (uiText)serializer.Deserialize(new StringReader(str));
             //ファイルを閉じる
             sr.Close();
+
+            return obj;
         }
+
     }
 
     [XmlRoot("string_table")]
     public class uiText
     {
         [XmlElement("string")]
-        public List<Texts> uiTexts { get; set; }
+        public List<Texts> UITexts { get; set; }
     }
 
     public class Texts
